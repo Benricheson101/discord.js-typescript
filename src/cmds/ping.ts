c1fd4e9b37c035e89988ce39775b6d41114e54b6
Ben Richeson <36977340+Benricheson101@users.noreply.github.com>
2020-11-12 02:06:11 -0500

    Update ping.ts

--- src/cmds/ping.ts
+++ src/cmds/ping.ts
@@ -9,5 +9,5 @@ export default new Command({
 }, async (client, message) => {
   const pre: number = Date.now()
   const msg = await message.channel.send('Pong!')
-  await msg.edit(`:ping_pong: Websocket ping: ${client.ws.ping}ms | Bot latency: ${Date.now() - pre}ms`)
+  await msg.edit(`:ping_pong: Websocket ping: ${client.ws.ping}ms | Message sent in: ${Date.now() - pre}ms`)
 })
